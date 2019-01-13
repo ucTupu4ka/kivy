0d25bbd56ceb40a5681dfa8ae1865d4b5f4d1556
ucTupu4ka <yaucTupu4ka@gmail.com>
2019-01-13 17:22:11 +0300

    Изучаем kivy, добавили кнопки сохранить и очистить

--- canvas.py
+++ canvas.py
@@ -1,13 +1,15 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from random import random
 from kivy.graphics import (Color, Ellipse, Rectangle, Line)
 
 
 class PainterWidget(Widget):
     def on_touch_down(self, touch):
         with self.canvas:
-            Color(0, 1, 0, 1)
+            Color(random(), random(), random(), random())
             rad = 30
             Ellipse(pos=(touch.x - rad / 2, touch.y - rad / 2), size=(rad, rad))
             touch.ud['line'] = Line(points=(touch.x, touch.y), width=15)
@@ -17,8 +19,20 @@ class PainterWidget(Widget):
 
 class PaintApp(App):
     def build(self):
-        return PainterWidget()
+        self.painter = PainterWidget()
+        parent = Widget()
+        parent.add_widget(self.painter)
 
+        parent.add_widget(Button(text='Clear', on_press=self.clear_canvas, size=(100, 50)))
+        parent.add_widget(Button(text='Save', on_press=self.save, size=(100, 50), pos=(100, 0)))
+        return parent
+
+    def clear_canvas(self, instance):
+        self.painter.canvas.clear()
+
+    def save(self, instance):
+        self.painter.size=(Window.size[0], Window.size[1])
+        self.painter.export_to_png('image.png')
 
 if __name__ == '__main__':
     PaintApp().run()
