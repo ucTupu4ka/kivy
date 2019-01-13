ccdcfabc02fa6de13d407918357e6bfc05f6e7b4
ucTupu4ka <yaucTupu4ka@gmail.com>
2019-01-13 11:56:28 +0300

    Изучаем kivy layout gridlayout

--- layout.py
+++ layout.py
@@ -2,17 +2,21 @@ from kivy.app import App
 from kivy.uix.button import Button
 
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.uix.gridlayout import GridLayout
 
 class BoxApp(App):
     def build(self):
-        bl = BoxLayout(orientation='horizontal', padding=[50])
+        gl = GridLayout(cols=10, padding=[30], spacing=3)
 
-        bl.add_widget(Button(text="Кнопка 1"))
-        bl.add_widget(Button(text="Кнопка 2"))
-        bl.add_widget(Button(text="Кнопка 3"))
+        for x in range(50):
+            gl.add_widget(Button(text="Кнопка"))
 
-        return bl
+        #bl = BoxLayout(orientation='horizontal', padding=[50], spacing=100)
+        #bl.add_widget(Button(text="Кнопка 1"))
+        #bl.add_widget(Button(text="Кнопка 2"))
+        #bl.add_widget(Button(text="Кнопка 3"))
+
+        return gl
 
 
 
