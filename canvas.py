5f992077ce63ebf24f8171083b9f5ed181ddb4ac
ucTupu4ka <yaucTupu4ka@gmail.com>
2019-01-13 17:00:17 +0300

    Изучаем kivy, рисуем линиии

--- canvas.py
+++ canvas.py
@@ -1,7 +1,8 @@
-from kivi.app import App
+from kivy.app import App
 from kivy.uix.widget import Widget
 
-from kivy.graphics import (Color, Ellipse, Rectangle, Line
+from kivy.graphics import (Color, Ellipse, Rectangle, Line)
+
 
 class PainterWidget(Widget):
     def __init__(self, **kwargs):
@@ -9,11 +10,17 @@ class PainterWidget(Widget):
 
         with self.canvas:
             Color(0, 1, 0, 1)
-            Ellipse(pos = (100, 100), size = (50, 50))
+            #Ellipse(pos=(100, 100), size=(50, 50))
+            #Rectangle(pos=(200, 200), size=(50, 50))
+            self.line = Line(points=(), width=10, close = True, joint = 'miter')
+
+    def on_touch_down(self, touch):
+        self.line.points += (touch.x, touch.y)
 
 class PaintApp(App):
     def build(self):
         return PainterWidget()
+
 
 if __name__ == '__main__':
     PaintApp().run()
