afd8c21a73890004403dbf40ee0b2b705e51b6bc
ucTupu4ka <yaucTupu4ka@gmail.com>
2019-01-13 15:08:20 +0300

    Изучаем kivy calculat

--- calculat.py
+++ calculat.py
@@ -1,37 +1,77 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'width', 400)
+Config.set('graphics', 'height', 500)
 
 
-
-class BoxApp(App):
+class CalculatApp(App):
     def build(self):
 
-        gl = GridLayout(cols=4, padding=[30], spacing=3)
-        gl.add_widget(Button(text="7"))
-        gl.add_widget(Button(text="8"))
-        gl.add_widget(Button(text="9"))
-        gl.add_widget(Button(text="x"))
 
-        gl.add_widget(Button(text="4"))
-        gl.add_widget(Button(text="5"))
-        gl.add_widget(Button(text="6"))
-        gl.add_widget(Button(text="-"))
+        self.formula = '0'
+        bl = BoxLayout(orientation='vertical', padding=[25])
+        gl = GridLayout(cols=4, spacing=3, size_hint=(1, .6))
 
-        gl.add_widget(Button(text="1"))
-        gl.add_widget(Button(text="2"))
-        gl.add_widget(Button(text="3"))
-        gl.add_widget(Button(text="+"))
+        self.lbl = Label(text="0",
+                         font_size=40,
+                         halign='right', valign='center',
+                         size_hint=(1, .4),
+                         text_size=(400 - 50, 500 * .4 - 50))
+        bl.add_widget(self.lbl)
+
+        gl.add_widget(Button(text="7", on_press=self.add_number))
+        gl.add_widget(Button(text="8", on_press=self.add_number))
+        gl.add_widget(Button(text="9", on_press=self.add_number))
+        gl.add_widget(Button(text="*", on_press=self.add_operation))
+
+        gl.add_widget(Button(text="4", on_press=self.add_number))
+        gl.add_widget(Button(text="5", on_press=self.add_number))
+        gl.add_widget(Button(text="6", on_press=self.add_number))
+        gl.add_widget(Button(text="-", on_press=self.add_operation))
+
+        gl.add_widget(Button(text="1", on_press=self.add_number))
+        gl.add_widget(Button(text="2", on_press=self.add_number))
+        gl.add_widget(Button(text="3", on_press=self.add_number))
+        gl.add_widget(Button(text="+", on_press=self.add_operation))
 
         gl.add_widget(Widget())
-        gl.add_widget(Button(text="0"))
-        gl.add_widget(Button(text=","))
-        gl.add_widget(Button(text="="))
+        gl.add_widget(Button(text="0", on_press=self.add_number))
+        gl.add_widget(Button(text=",", on_press=self.add_number))
+        gl.add_widget(Button(text="=", on_press=self.calc_result))
 
-        return gl
+        bl.add_widget(gl)
+        return bl
+
+    def add_number(self, instance):
+        if(self.formula == "0"):
+            self.formula = ""
+
+        self.formula += str(instance.text)
+        self.update_lable()
+
+    def add_operation(self, instance):
+        if(str(instance.text).lower() == "x"):
+            self.formula += "*"
+        else:
+            self.formula += str(instance.text)
+
+        self.update_lable()
+
+    def calc_result(self, instance):
+        self.lbl.text = str(eval(self.lbl.text))
+        self.formula = "0"
+
+    def update_lable(self):
+        self.lbl.text = self.formula
+
 
 if __name__ == "__main__":
-    BoxApp().run()
+    CalculatApp().run()
