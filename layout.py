421a69bd6600c52a68d454e375dfb5c4e6e35c4a
ucTupu4ka <yaucTupu4ka@gmail.com>
2019-01-13 11:43:58 +0300

    Изучаем kivy layout boxlayout

--- layout.py
+++ layout.py
@@ -4,11 +4,16 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
 
-
-
 class BoxApp(App):
     def build(self):
-        return Button()
+        bl = BoxLayout(orientation='horizontal', padding=[50])
+
+        bl.add_widget(Button(text="Кнопка 1"))
+        bl.add_widget(Button(text="Кнопка 2"))
+        bl.add_widget(Button(text="Кнопка 3"))
+
+        return bl
+
 
 
 if __name__ == "__main__":
