1780f9ff6ad6d6b6737dcbfcd784f2f76c8cdf0e
ucTupu4ka <yaucTupu4ka@gmail.com>
2019-01-13 14:04:45 +0300

    Изучаем kivy layout anchorlayout Добавили форму авторизации

--- layout.py
+++ layout.py
@@ -9,19 +9,18 @@ from kivy.uix.anchorlayout import AnchorLayout
 
 class BoxApp(App):
     def build(self):
-        #al = AnchorLayout(anchor_x = 'center', anchor_y = 'center')
-        #al.add_widget(Button(text="Кнопка 1", size_hint=[.5, .5]))
+        al = AnchorLayout(anchor_x = 'center', anchor_y = 'center')
 
         #gl = GridLayout(cols=10, padding=[30], spacing=3)
-
         #for x in range(50):
         #    gl.add_widget(Button(text="Кнопка"))
 
-        #bl = BoxLayout(orientation='horizontal', padding=[50], spacing=100)
-        #bl.add_widget(Button(text="Кнопка 1"))
-        #bl.add_widget(Button(text="Кнопка 2"))
-        #bl.add_widget(Button(text="Кнопка 3"))
+        bl = BoxLayout(orientation='vertical', size_hint=[.4, .4])
+        bl.add_widget(TextInput())
+        bl.add_widget(TextInput())
+        bl.add_widget(Button(text="Enter"))
 
+        al.add_widget(bl)
         return al
 
 
