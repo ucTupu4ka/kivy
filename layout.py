98323cbb04e01368e8aa78fbba946572487098a1
ucTupu4ka <yaucTupu4ka@gmail.com>
2019-01-13 13:54:11 +0300

    Изучаем kivy layout anchorlayout

--- layout.py
+++ layout.py
@@ -1,22 +1,28 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+
 
 class BoxApp(App):
     def build(self):
-        gl = GridLayout(cols=10, padding=[30], spacing=3)
+        #al = AnchorLayout(anchor_x = 'center', anchor_y = 'center')
+        #al.add_widget(Button(text="Кнопка 1", size_hint=[.5, .5]))
 
-        for x in range(50):
-            gl.add_widget(Button(text="Кнопка"))
+        #gl = GridLayout(cols=10, padding=[30], spacing=3)
+
+        #for x in range(50):
+        #    gl.add_widget(Button(text="Кнопка"))
 
         #bl = BoxLayout(orientation='horizontal', padding=[50], spacing=100)
         #bl.add_widget(Button(text="Кнопка 1"))
         #bl.add_widget(Button(text="Кнопка 2"))
         #bl.add_widget(Button(text="Кнопка 3"))
 
-        return gl
+        return al
 
 
 
